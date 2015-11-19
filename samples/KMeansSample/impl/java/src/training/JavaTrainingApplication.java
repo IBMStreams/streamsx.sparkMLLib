3328c1b13324575e344b26fa0453f368dc182e19
Ankit Pasricha <ankitp@ca.ibm.com>
2015-11-19 15:46:21 -0500

    issue #15

---
+++ samples/KMeansSample/impl/java/src/training/JavaTrainingApplication.java
@@ -0,0 +1,39 @@
+import java.io.File;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+
+public class JavaTrainingApplication {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkStreamsSampleTrainingApplication");
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+		
+		JavaRDD<String> lines = jsc.textFile("random_2d_training.csv");
+		JavaRDD<Vector> parsedData = lines.map(
+	      new Function<String, Vector>() {
+			@Override
+			public Vector call(String s) {
+			    String[] sarray = s.split(",");
+		          double[] values = new double[sarray.length];
+		          for (int i = 0; i < sarray.length; i++) {
+		            values[i] = Double.parseDouble(sarray[i]);
+		          }
+		          return Vectors.dense(values);
+			}
+	      }
+	    );
+		parsedData.cache();
+		
+	    int numClusters = 10;
+	    int numIterations = 20;
+	    KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
+	    clusters.save(jsc.sc(), "/some/path/kmeans_model");
+	    jsc.close();
+	}
+}
